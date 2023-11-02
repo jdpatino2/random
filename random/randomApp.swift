78b54b72b765b17aa77aeac64bc2ce84b1d7d718
Juan <jdpatino2@cps.edu>
2023-11-02 08:26:07 -0500

    Initial Commit

---
+++ random/randomApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomApp.swift
+//  random
+//
+//  Created by Patino, Juan D on 11/2/23.
+//
+
+import SwiftUI
+
+@main
+struct randomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
